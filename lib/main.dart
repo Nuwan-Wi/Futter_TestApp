3fd20c17247e2c2eadf692a73bb1c168c3b9b2ba
Nuwan-Wi <nuwanwijeweera@gmail.com>
2022-09-28 15:16:36 +0530

    Botem Navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,8 +33,22 @@ class _RootPageState extends State<RootPage> {
         title: const Text('Flutter'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        ),
+        onPressed: () {
+          debugPrint('Floating Action Button');
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+      ),
     );
   }
 }
